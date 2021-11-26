21c2c7ba45a26cc30f535a657c68fd4ec7db6555
Hufan <hufany21@gmail.com>
2021-11-26 10:11:17 -0500

    update getAllReservations  function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -61,7 +61,7 @@ exports.getUserWithId = getUserWithId;
     VALUES ($1, $2, $3)
     RETURNING *;
   `), [user.name, user.email, user.password])
-  .then((result) => result.rows) //change from console.log
+  .then((result) => console.log(result.rows))
       .catch((err) => {
         console.log(err.message);
       });
@@ -76,8 +76,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool
+  .query ((`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+  `), [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    return (err.message);
+  });
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
